57e7d2cb0c7843ced3e419a72d78260092e311be
Chris Desmarais <desmarais_chris@hotmail.com>
2023-04-20 18:34:16 -0600

    Moved the provider up a level to simplify the app.jsx

--- motivatingExample/src/App.jsx
+++ motivatingExample/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,31 +6,22 @@ import './App.css'
 import Footer from './components/footer'
 import Login from './components/loginPage'
 import Main from './components/mainPage'
+import { UserContext } from './context/userContext'
 
-import UserContextProvider, { UserContext } from './context/userContext'
+function App() {
+  const {user, setUser} = useContext(UserContext)
 
-function InternalApp(){
-  const {user} = useContext(UserContext)
-
-  return <>
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch', 
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch', 
       position: 'fixed', width: '100%', height: '100%', left: 0, top: 0}}>
       <div style={{flexGrow: 1, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
         {user == null ? 
-          <Login /> :
-          <Main />
+          <Login setUser={setUser}/> :
+          <Main user={user}/>
         }
       </div>
-      <Footer />
+      <Footer user={user} setUser={setUser} />
     </div>
-  </>
-}
-
-function App() {
-  return (
-    <UserContextProvider>
-      <InternalApp />
-    </UserContextProvider>
   )
 }
 
